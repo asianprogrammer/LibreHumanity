7f22413490b3f831299b7855a69cd1a891a6dcca
Me <sorry@404.com>
2025-04-12 17:42:41 +0600

    Code Bug Fixed

--- charts.js
+++ charts.js
@@ -906,7 +906,7 @@ async function analyzeWithAI(prompt) {
     const controller = new AbortController();
     const timeoutId = setTimeout(() => controller.abort(), 30000); // 30 second timeout
     
-    const response = await fetch('/.netlify/functions/your-function-name', {
+    const response = await fetch('/.netlify/functions/analyzeWithAI', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
